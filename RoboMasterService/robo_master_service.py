0499e927bdcbf8018366f4f53022ef4cb0a40e01
rocknio <rocknio@gmail.com>
2021-01-27 13:24:19 +0800

    bug fix

--- RoboMasterService/robo_master_service.py
+++ RoboMasterService/robo_master_service.py
@@ -10,9 +10,6 @@ from robo_master_protocol.ir.robotic_ir import RoboticIr
 from robo_master_protocol.robotic_conn.robotic_connection import RoboticConn
 from tennis_detect_service.tennis_detect import TennisDetectService
 from settings import low_color, high_color, robot_master_sn, robot_host, robot_port
-from RoboMasterService.robo_master_stats import RoboMasterStats
-
-RobotMasterStat = RoboMasterStats()
 
 
 class RoboMasterService:
